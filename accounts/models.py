286186ba1daad7dc4488a3adabb4b32ca2a93b53
MrSinxx <sinaleylaz@yahoo.com>
2023-09-01 10:55:10 +0330

    feat: CustomUser class

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,14 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
+
+class CustomUser(AbstractUser):
+    ACCOUNT_TYPES = (("VIP", "VIP"), ("NORMAL", "Normal"))
+    account_type = models.CharField(
+        choices=ACCOUNT_TYPES, default="NORMAL", max_length=10
+    )
+    profile_image = models.ImageField(upload_to="users/", blank=True, null=True)
+    additional_info = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.username

08e4b4655f78cc1927a951354f0dc54b64d8279b
MrSinxx <sinaleylaz@yahoo.com>
2023-09-01 10:34:17 +0330

    feat: model for Song

--- accounts/models.py
+++ accounts/models.py
@@ -15,3 +15,15 @@ class Artist(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Song(models.Model):
+    title = models.CharField(max_length=255)
+    artists = models.ManyToManyField(Artist)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    cover_photo = models.ImageField(upload_to="songs/", blank=True, null=True)
+    audio_file = models.FileField(upload_to="songs/")
+    genres = models.ManyToManyField(Genre)
+
+    def __str__(self):
+        return self.title

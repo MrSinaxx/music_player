e3a6cd4c4e8b760e6a302569cb809edd49ec4e89
MrSinxx <sinaleylaz@yahoo.com>
2023-09-01 10:24:26 +0330

    feat: str method for Genre & Artist

--- accounts/models.py
+++ accounts/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class Artist(models.Model):
     name = models.CharField(max_length=255)
     bio = models.TextField()
     image = models.ImageField(upload_to="artists/")
+
+    def __str__(self):
+        return self.name

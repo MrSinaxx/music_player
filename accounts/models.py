506c7197f0a69bfe5635ba27f88b099bb8b714e9
MrSinxx <sinaleylaz@yahoo.com>
2023-09-01 10:58:45 +0330

    feat: like and comment class

--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,24 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Like(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    song = models.ForeignKey("music.Song", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ("user", "song")
+
+    def __str__(self):
+        return f"{self.user.username} liked {self.song.title}"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    song = models.ForeignKey("music.Song", on_delete=models.CASCADE)
+    text = models.TextField()
+    approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Comment by {self.user.username} on {self.song.title}"

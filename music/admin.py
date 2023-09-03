198577f2420ddcbfe559de205daa025f4e491942
MrSinxx <sinaleylaz@yahoo.com>
2023-09-03 21:04:21 +0330

    feat: Artist admin

--- music/admin.py
+++ music/admin.py
@@ -7,3 +7,10 @@ class GenreAdmin(admin.ModelAdmin):
     list_display = ("name",)
     search_fields = ("name",)
     ordering = ("name",)
+
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = ("name", "bio")
+    search_fields = ("name", "bio")
+    ordering = ("name",)

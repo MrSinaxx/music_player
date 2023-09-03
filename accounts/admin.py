643c15de21cf98ea252c8a73fca354f9033689f5
MrSinxx <sinaleylaz@yahoo.com>
2023-09-03 21:01:08 +0330

    feat: CustomUserAdmin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import CustomUser, Like, Comment
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ("username", "email", "account_type", "date_joined", "last_login")
+    list_filter = ("account_type", "is_staff", "date_joined")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+    date_hierarchy = "date_joined"

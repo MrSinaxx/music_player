bacd2dbb7221659a3a5a0223448bea9a203e490b
MrSinxx <sinaleylaz@yahoo.com>
2023-09-03 21:18:11 +0330

    feat: AUTHENTICATION_BACKENDS

--- config/settings.py
+++ config/settings.py
@@ -125,3 +125,8 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "accounts.CustomUser"
+
+AUTHENTICATION_BACKENDS = [
+    "account.backends.EmailOrUsernameModelBackend",
+    "django.contrib.auth.backends.ModelBackend",
+]

88c6a7aa5dc7005baaba47a2859e491e96d4283d
MrSinxx <sinaleylaz@yahoo.com>
2023-09-05 12:55:20 +0330

    feat: simple views to work with the urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, "accounts/home/home.html")
+
+
+def otp(request):
+    return render(request, "accounts/login/otp.html")
+
+
+def login(request):
+    return render(request, "accounts/login/login.html")
+
+
+def signup(request):
+    return render(request, "accounts/signup/signup.html")
+
+
+def profile(request):
+    return render(request, "accounts/profile/profile.html")
+
+
+def myplaylists(request):
+    return render(request, "accounts/myplaylists/myplaylists.html")

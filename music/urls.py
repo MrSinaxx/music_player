647b4585bc2ae7198005f2abde60b24803583d7f
MrSinxx <sinaleylaz@yahoo.com>
2023-09-05 12:56:35 +0330

    feat: simple urls to work on the front

---
+++ music/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("playlist-detail/", playlist_detail, name="playlist-detail"),
+    path("song-detail/", song_detail, name="song-detail"),
+]
